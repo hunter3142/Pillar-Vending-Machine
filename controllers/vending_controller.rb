7190e56a39bc9e0df2b5cfb4bb5c142611156bad
Mathew <hunter3142@gmail.com>
2017-11-27 00:21:21 -0500

    Return Coins && Sold Out

--- controllers/vending_controller.rb
+++ controllers/vending_controller.rb
@@ -22,9 +22,10 @@ class VendingController
 		puts "           2=> #{@chips.name}(#{@chips.inventory_count}): $#{'%.02f' % @chips.price}"
 		puts "           3=> #{@candy.name}(#{@candy.inventory_count}): $#{'%.02f' % @candy.price}"
     if coins.inserted_coin_total == 0
-      print "#{displays.current_display}\n"
+      puts "#{displays.current_display}\n"
     elsif coins.inserted_coin_total > 0 
-      print "Money Entered: $#{'%.02f' % coins.inserted_coin_total}\n"
+      puts " Money Entered: $#{'%.02f' % coins.inserted_coin_total}"
+      puts "(enter RETURN to get your money back)\n"
     end
 
     user_input = gets.chomp.downcase
@@ -50,10 +51,17 @@ class VendingController
         item_output(@chips)
       when "3"
         item_output(@candy)
+      when "return"
+        system "clear"
+        puts "           Returned: $#{coins.inserted_coin_total}"
+        system "sleep 1"
+        system "clear"
+        coins.reset_totals
+        main_menu
 # >>>>>>>>>>_____FOR INVALID INPUT_____
       else
         system "clear"
-        print "           Invalid Coin/Item Number\n"
+        puts "           Invalid Coin/Item Number\n"
         system "sleep 1"
         system "clear"
         main_menu
@@ -61,7 +69,13 @@ class VendingController
 	end
 # >>>>>>>>>>_____ITEM OUTPUTS_____
   def item_output(item)
-    if (coins.inserted_coin_total == item.price)
+    if (item.inventory_count == 0)
+      system "clear"
+      puts "           SOLD OUT"
+      system "sleep 1"
+      system "clear"
+      main_menu
+    elsif (coins.inserted_coin_total == item.price)
       system "clear"
       item.remove_item
       @coins.reset_totals
